fd8f9add54c46ad50993cdef890a48150ddc773d
Robadev254 <anthonioroberto2001@gmail.com>
2023-03-17 03:02:16 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,8 @@ for (c = '0'; c <= '9'; c++)
 for (i = '0'; i <= '9'; i++)
 {
 if (c < i)
-
+{
 putchar(c);
-}
 putchar(i);
 if ((c != '8') || (c == '8' && i != '9'))
 {	
@@ -26,3 +25,4 @@ putchar(' ');
 putchar('\n');
 return (0);
 }
+
