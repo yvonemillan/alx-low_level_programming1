343f7e182bf83dfeadf2184524fcf5abea1facdd
Robadev254 <anthonioroberto2001@gmail.com>
2023-03-17 02:48:15 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	char c;
-	for (c = '0'; c <= '9'; c++)
-	putchar(c);
-	putchar('\n');
-	return (0);
+char c;
+for (c = '0'; c <= '9'; c++)
+putchar(c);
+putchar('\n');
+return (0);
 }
 
