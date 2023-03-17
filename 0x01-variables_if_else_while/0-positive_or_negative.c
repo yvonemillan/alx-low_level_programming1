ffbd42aeea41100cb0df8356ce7724ff883b64a4
Robadev254 <anthonioroberto2001@gmail.com>
2023-03-17 00:55:17 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main - takesrandom number into n and execute
+ * condition given must be satiafied
+ * Return: always give 0
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	printf("%d is positive\n", n);
+
+	if (n == 0)
+	printf("%d is zero\n", n);
+
+	if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+
+	return (0);
+
+}
+
